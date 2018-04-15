172c0bcacadcca1fc7d0b5949a7b26ef4133cde1
rt2zz <zack@root-two.com>
2018-04-15 00:52:14 -0700

    (types): add typescript

--- packages/unstated-persist/src/unstated-persist.d.ts
+++ packages/unstated-persist/src/unstated-persist.d.ts
@@ -1,1 +1,13 @@
-// @TODO
+import { Container } from 'unstated'
+
+type PersistConfig = {
+  key: string,
+  version: number,
+  storage: any, // @TODO add storage typing
+}
+
+export class PersistContainer<State extends object> extends Container{
+  persist: PersistConfig
+}
+
+export const isBootstrapped: (Container) => boolean
